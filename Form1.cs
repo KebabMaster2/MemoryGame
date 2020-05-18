f94326fd77b43dfefa03d4c6a300f6c56dc0c9d4
Valdis <valpivars@gmail.com>
2020-05-18 16:58:37 +0300

    version 1

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,35 @@ namespace MemoryGame
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+        }
+        private void InitializeGrid()
+        {
+            Label label;
+            Grid.BackColor = Color.LightSteelBlue;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Outset;
+            //webdings e, d, k, b, N, !, &, $
+
+            for (int c = 0; c < 4; c++)
+            {
+                for(int r = 0; r < 4; r++)
+                {
+                    label = new Label
+                    {
+                        Dock = DockStyle.Fill,
+                        ForeColor = Color.Black,
+                        AutoSize = false,
+                        TextAlign=ContentAlignment.MiddleCenter,
+                        Font = new Font("Webdings", 72, FontStyle.Bold),
+                        Text = "e"
+                    };
+                    Grid.Controls.Add(label, c, r);
+                }
+            }
+
+            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 0);
+
+
         }
     }
 }
